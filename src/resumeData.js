ae1ff63f778553dcc8c0188cb1662bf45b7e3c5a
David_Molina <juandavidmolina0622@gmail.com>
2024-05-25 20:11:42 -0500

    first commit

---
+++ src/resumeData.js
@@ -0,0 +1,97 @@
+let resumeData = {
+    "imagebaseurl":"",
+    "name": "David Molina",
+    "role": "Frontend Developer",
+    // "linkedinId":"Your LinkedIn Id",
+    // "skypeid": "Your skypeid",
+    "roleDescription": " Who enjoys exploring various aspects of frontend development and learning about new technologies. In my free time I like to study and delve into different areas of web development.",
+    "socialLinks":[
+        // {
+        //   "name":"linkedin",
+        //   "url":"https://www.linkedin.com/in/rahul-bhatia-67ba08121/",
+        //   "className":"fa fa-linkedin"
+        // },
+        {
+          "name":"github",
+          "url":"https://github.com/David-Auditore",
+          "className":"fa fa-github"
+        },
+        // {
+        //   "name":"skype",
+        //   "url":"http://twitter.com/",
+        //   "className":"fa fa-twitter"
+        // }
+      ],
+    "aboutme":"I am a young man passionate about software programming and computer systems, always looking to learn new things and improving what I already know, to give the best version of myself.",
+    "address":"Colombia",
+    "website":"juandavidmolina0622@gmail.com",
+    "education":[
+      {
+        "UniversityName":"Centro de comercio y turismo SENA",
+        "specialization":"Analisis y desarrollo de Software",
+        "MonthOfPassing":"Dec",
+        "YearOfPassing":"2023",
+        "Achievements":""
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Parque del Cafe",
+        "specialization":"Web Developer",
+        "MonthOfLeaving":"",
+        "YearOfLeaving":"Actual",
+      },
+    ],
+    "skillsDescription":"",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"reactjs"
+      },
+      {
+        "skillname":"javascript"
+      },
+      {
+        "skillname":"c-sharp"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"Dashboard Parque del Cafe",
+        "description":"App Web",
+        "imgurl":"images/parcafe.png"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/parcafe.png"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/parcafe.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/parcafe.png"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
